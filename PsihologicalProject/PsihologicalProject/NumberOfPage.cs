4e8d39df3017cd2c9c911db051768e22f276315f
mrtnvm <mrtnv.m@gmail.com>
2014-03-30 19:05:44 +0400

    Version 1.1 Тест Юнга Логика без оформления

---
+++ PsihologicalProject/PsihologicalProject/NumberOfPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsihologicalProject
+{
+    class NumberOfPage
+    {
+        public static int Get(object t)
+        {
+            string buf = t.ToString();
+            int Number = 0;
+            int i = buf.Length - 1;
+            while (Char.IsDigit(buf[i]))
+            {
+                Number = Int32.Parse(buf[i].ToString() + ((Number != 0)?(Number.ToString()):("")));
+                i--;
+            }
+            var messageDialog = new Windows.UI.Popups.MessageDialog("Number = " + Number);
+            return Number;
+        }
+    }
+}
---
+++ PsihologicalProject/PsihologicalProject/TY.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsihologicalProject
+{
+    class TY
+    {
+        TY()
+        {
+            for (int i = 0; i < ArrayOfResults.Length; i++)
+                ArrayOfResults[i] = -1;
+        }
+        public static int Result
+        {
+            get
+            {
+                int cnt = 0;
+                foreach (int i in ArrayOfResults)
+                    cnt += i;
+                return cnt * 5;
+            }
+        }
+        private static int Count = 20;
+        private static int[] _array = new int[Count];
+        public static int[] ArrayOfResults
+        {
+            get
+            {
+                return _array;
+            }
+            set { _array = value; }
+        }
+
+        public static string GetContent()
+        {
+            string buf = "";
+            for (int m = 0; m < 20; m++)
+            {
+                buf += (m+1) + ") " + TY.ArrayOfResults[m] + "  ";
+            }
+            return buf;
+        }
+    }
+}
